5464ad5ad952f8cbc8a86941df0e158f4285b3f5
Richard Yance <yance0022@gmail.com>
2019-04-07 01:21:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,6 +10,16 @@ class Student
   end
 
   def self.create_table
+    def self.create_table
+  sql =  <<-SQL 
+    CREATE TABLE IF NOT EXISTS songs (
+      id INTEGER PRIMARY KEY, 
+      name TEXT, 
+      grade INTEGER
+      )
+      SQL
+  DB[:conn].execute(sql) 
+end
   end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
