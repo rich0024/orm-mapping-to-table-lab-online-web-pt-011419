0555172b0efb1cbd40ec72c913a9e3c8a402b07c
Richard Yance <yance0022@gmail.com>
2019-04-07 01:13:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,12 @@
 class Student
 
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name, grade, id = some_id)
+    @name = name
+    @grade = grade
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
