5ba229d34564132b2bc0d5f0b73adcc5ff67bcf4
Richard Yance <yance0022@gmail.com>
2019-04-07 01:18:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,6 +8,9 @@ class Student
     @name = name
     @grade = grade
   end
+
+  def self.create_table
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
